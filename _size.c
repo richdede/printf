09a881c64756a21ca417b871d2810e4ca085bc9c
DD-SQUARED <drewnare29@gmail.com>
2023-08-19 06:15:03 -0700

    printf_Andrew

---
+++ _size.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * _size - Calculates the size so it can cast the argument
+ * @format: Formatted string in which to print the arguments
+ * @i: The list of arguments to be printed.
+ *
+ * Return: Precision.
+ */
+int _size(const char *format, int *i)
+{
+	int curr_i = *i + 1;
+	int size = 0;
+
+	if (format[curr_i] == 'l')
+		size = S_LONG;
+	else if (format[curr_i] == 'h')
+		size = S_SHORT;
+
+	if (size == 0)
+		*i = curr_i - 1;
+	else
+		*i = curr_i;
+
+	return (size);
+}
+
