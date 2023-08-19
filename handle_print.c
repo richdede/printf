36de24dcf09f32ca1599099239e73069f369f426
DD-SQUARED <drewnare29@gmail.com>
2023-08-19 07:23:44 -0700

    printf_Andrew_Update

--- handle_print.c
+++ handle_print.c
@@ -1,17 +1,17 @@
 #include "main.h"
 /**
- * handle - Argument printed based of its type
- * @fmt: Formatted string prints the arguments.
- * @list: Printed arguments in a list.
+ * handle_print - Prints an argument based on its type
+ * @fmt: Formatted string in which to print the arguments.
+ * @list: List of arguments to be printed.
  * @ind: ind.
- * @buffer: Print is handled by the buffer array.
- * @flags: Calculation of the active flags
- * @width: function get is used to fetch the length.
+ * @buffer: Buffer array to handle print.
+ * @flags: Calculates active flags
+ * @width: get width.
  * @precision: Precision specification
  * @size: Size specifier
  * Return: 1 or 2;
  */
-int handle(const char *fmt, int *ind, va_list list, char buffer[],
+int handle_print(const char *fmt, int *ind, va_list list, char buffer[],
 	int flags, int width, int precision, int size)
 {
 	int i, unknow_len = 0, printed_chars = -1;
